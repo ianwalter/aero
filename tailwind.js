b3dc830d3ad65e82320d0c2d452837eb18d26da0
Ian Walter <public@iankwalter.com>
2019-02-03 16:01:55 -0500

    Removing unnecessary font weights

--- tailwind.js
+++ tailwind.js
@@ -280,15 +280,15 @@ module.exports = {
   */
 
   fontWeights: {
-    'hairline': 100,
-    'thin': 200,
+    // 'hairline': 100,
+    // 'thin': 200,
     'light': 300,
     'normal': 400,
     'medium': 500,
     'semibold': 600,
     'bold': 700,
-    'extrabold': 800,
-    'black': 900,
+    // 'extrabold': 800,
+    // 'black': 900,
   },
 
 
