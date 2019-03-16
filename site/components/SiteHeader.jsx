3774cb2cf007a89346cfeff7bab062874508498e
Ian Walter <public@iankwalter.com>
2019-03-16 13:33:07 -0400

    Customizing tippy theme

--- site/components/SiteHeader.jsx
+++ site/components/SiteHeader.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Tippy from '@tippy.js/react'
-import 'tippy.js/themes/light-border.css'
 import NavMenu from './NavMenu'
 
 export default function SiteHeader () {
   return (
-    <header className="bg-grey-lighter h-12 pt-4 text-center font-semibold border-b border-grey-light">
+    <header className="bg-grey-lighter h-16 pt-5 text-center font-semibold border-b border-grey-light">
       <Tippy
         content={<NavMenu />}
         interactive={true}
@@ -14,7 +13,7 @@ export default function SiteHeader () {
         size="large"
         theme="light-border"
       >
-        <span className="cursor-pointer">aero</span>
+        <span className="cursor-pointer text-xl">aero</span>
       </Tippy>
     </header>
   )
