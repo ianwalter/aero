3488a771d9b5c7579f1ecb28d0a0f616a9bc30c4
Ian Walter <public@iankwalter.com>
2019-04-20 22:38:26 -0400

    Adding -p flag to build:site script

--- webpack.config.js
+++ webpack.config.js
@@ -4,65 +4,68 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = (_, { mode = 'development' }) => ({
-  mode,
-  entry: './site/main.jsx',
-  output: {
-    path: join(__dirname, 'site/dist'),
-    filename: 'js/[name].[contenthash].js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: 'site/index.html' }),
-    ...(mode === 'production' ? [
-      new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' })
-    ] : [
-      new webpack.HotModuleReplacementPlugin()
-    ])
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(jsx?)$/,
-        include: join(__dirname, 'site'),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          {
-            loader: '@fullhuman/purgecss-loader',
-            options: {
-              content: [join(__dirname, 'site/**/*.jsx')]
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                require('tailwindcss')('./tailwind.js'),
-                require('autoprefixer')()
-              ]
-            }
-          }
-        ]
+module.exports = (_, { mode = 'development' }) => {
+  const isProduction = mode === 'production'
+  return {
+    mode,
+    entry: './site/main.jsx',
+    output: {
+      path: join(__dirname, 'site/dist'),
+      filename: `js/[name].[${isProduction ? 'contenthash' : 'hash'}.js`
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom'
       }
-    ]
-  },
-  devServer: {
-    port: 9876,
-    contentBase: './site/dist',
-    hot: true,
-    historyApiFallback: true,
-    disableHostCheck: true
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: 'site/index.html' }),
+      ...(isProduction ? [
+        new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' })
+      ] : [
+        new webpack.HotModuleReplacementPlugin()
+      ])
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(jsx?)$/,
+          include: join(__dirname, 'site'),
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            {
+              loader: '@fullhuman/purgecss-loader',
+              options: {
+                content: [join(__dirname, 'site/**/*.jsx')]
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  require('tailwindcss')('./tailwind.js'),
+                  require('autoprefixer')()
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      port: 9876,
+      contentBase: './site/dist',
+      hot: true,
+      historyApiFallback: true,
+      disableHostCheck: true
+    }
   }
-})
+}
